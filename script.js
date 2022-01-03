6f55c0e796949c0f2823d5b9162ec15f6b173940
Gustavo Rondello <gustavorondello99@outlook.com>
2022-01-03 19:21:37 -0300

    feito requisito 1 a 3

---
+++ script.js
@@ -0,0 +1,19 @@
+window.onload = function () {
+  const corPrincipal = document.getElementById('rgb-color');
+  const bola = document.querySelector('.ball');
+
+  function randomizeRGB() {
+    const valorRGB = [0, 1, 2];
+    for (let i = 0; i <= valorRGB.length - 1; i += 1) {
+      // https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+
+      const min = Math.ceil(0);
+      const max = Math.floor(255);
+      let valorGerado = Math.floor(Math.random() * (max - min + 1)) + min;
+      valorRGB[i] = valorGerado;
+    }
+    corPrincipal.innerHTML += '(' + valorRGB + ')';
+  }
+
+  randomizeRGB();
+};
